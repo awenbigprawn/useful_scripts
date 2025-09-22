edf9a5e1a8fe41e2f4a54ed432b4b7462da51954
yuwen shen <yuwen323@outlook.com>
2025-09-22 08:09:20 +0000

    add gti as git

--- system_tools_setup/setup_git.sh
+++ system_tools_setup/setup_git.sh
@@ -12,3 +12,24 @@ git config --global alias.cs "commit -s"
 
 echo "Git configuration has been successfully applied."
 
+# to make git = gti
+ALIAS_CMD="alias gti='git'"
+
+# the files to edit
+CONFIG_FILES="$HOME/.bashrc $HOME/.profile $HOME/.zshrc"
+
+for FILE in $CONFIG_FILES; do
+    if [ -f "$FILE" ]; then
+        # check if alias already exist
+        if grep -Fxq "$ALIAS_CMD" "$FILE"; then
+            echo "Alias already exists in $FILE"
+        else
+            echo "$ALIAS_CMD" >> "$FILE"
+            echo "Alias added to $FILE"
+        fi
+    fi
+done
+
+# make available in current shell
+echo "alias gti='git' finished, source ~/.bashrc to enable."
+
